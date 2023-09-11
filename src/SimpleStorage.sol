b46d8e5449f33c64a5bf46c2d3f15f7afe4f018a
Przemo <przemo06133@gmail.com>
2023-09-11 21:58:43 +0200

    Init

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract SimpleStorage {
+    uint256 favNum;
+    uint256[] favNums;
+
+    struct Person {
+        uint256 favNum;
+        string name;
+    }
+
+    Person[] public people;
+
+    mapping(string => uint256) public nameToFavNum;
+
+    function addPerson(uint256 _favNum, string memory _name) public {
+        people.push(Person({favNum: _favNum, name: _name}));
+        nameToFavNum[_name] = _favNum;
+    }
+
+    function store(uint256 _favNum) public {
+        favNum = _favNum;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favNum;
+    }
+}
